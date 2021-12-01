99ced1cca5f7726ec10d6b917ed95a9e61caba62
TJ Tahmassebi <tjtahmas@gmail.com>
2021-11-30 19:32:39 -0500

    finishing models, fixing package

---
+++ models/Post.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require('../config/connection')
+
+class Post extends Model{ }
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        // date_created: {
+        //     //Not entirely sure about how to save the time here
+        //     type: DataTypes.TIME,
+        //     allowNull: false,
+        // },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        sequelize,
+        //I think maybe I just make timestamps true. That makes sense to me. 
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
